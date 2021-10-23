6e33056ce4138af7b5dfce256f105ce5f439e501
suyeong <86659054+jangsuyeongg@users.noreply.github.com>
2021-10-23 11:42:49 +0900

    1023 페이지수정
    1023 페이지수정

--- leaf/src/main/java/com/leaf/myapp/controller/AdminNoticeController.java
+++ leaf/src/main/java/com/leaf/myapp/controller/AdminNoticeController.java
@@ -10,13 +10,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.leaf.myapp.service.AdminNoticeService;
 import com.leaf.myapp.vo.AdminNoticeVO;
+import com.leaf.myapp.vo.AdminPageVO;
+import com.leaf.myapp.vo.NoticePagingVO;
 import com.leaf.myapp.vo.NoticeVO;
 
 @Controller
-public class AdminNoticeController {
+
+  public class AdminNoticeController {
 	@Inject
 	AdminNoticeService adminnoticeService;
-
+/*
 	//게시글 리스트
     @RequestMapping("/adminnoticeMain")
     public ModelAndView adminnoticeList() {
@@ -25,11 +28,34 @@ public class AdminNoticeController {
     	mav.setViewName("adminNotice/noticeMain");
 		return mav;      
    }
+    
+ * */
+	//게시글 리스트
+    @RequestMapping("/adminnoticeMain")
+    public ModelAndView noticePageSelect(AdminPageVO pVo) {
+    	ModelAndView mav = new ModelAndView();
+       //총레코드수
+    	pVo.setTotalRecord(adminnoticeService.totalRecordCount(pVo));
+    	mav.addObject("pVo",pVo);   
+		System.out.println("total record: " + pVo.getTotalRecord());
+		mav.addObject("list", adminnoticeService.noticePageSelect(pVo));		
+		System.out.println("list" + adminnoticeService.noticePageSelect(pVo).get(0).getAdmin_no());
+		mav.setViewName("adminNotice/noticeMain");
+		return mav;      
+   }
+    
 	//게시글보기
 	@RequestMapping("/adminnoticeDetail")
 	public ModelAndView noticeDetail(int no, AdminNoticeVO vo) {
-		ModelAndView mav = new ModelAndView();	
-		mav.addObject("adminnoticeVo", adminnoticeService.noticeView(no));		
+		//////////김승규용
+		System.out.println(no);
+		System.out.println(adminnoticeService.noticeView(no).getAdmin_title());
+		System.out.println(vo.getAdmin_title());
+		vo = adminnoticeService.noticeView(no);
+		//////////
+		ModelAndView mav = new ModelAndView();
+		vo.setAdmin_hit(adminnoticeService.hitCount(vo));
+		mav.addObject("adminnoticeVo", vo);		
 		mav.setViewName("adminNotice/adminnoticeDetail");
 		return mav;	
 	}
@@ -44,8 +70,39 @@ public class AdminNoticeController {
 		ModelAndView mav = new ModelAndView();
 		//vo.setUserid((String)session.getAttribute("userid"));		
 		adminnoticeService.adminnoticeWriteOk(vo);
-		mav.addObject(vo);
-		mav.setViewName("adminNotice/noticeMain");
+		mav.setViewName("redirect:adminnoticeMain");
 		     return mav;
 		}
+	
+	//글수정폼
+	@RequestMapping("/adminnoticeEdit")
+	public ModelAndView noticeEdit(int no){
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("vo", adminnoticeService.noticeView(no));
+		mav.setViewName("adminNotice/noticeEdit");
+		return mav;
+	}
+	
+	//글수정
+	@RequestMapping(value="/adminnoticeEditOk", method=RequestMethod.POST)
+	public ModelAndView noticeEditOk(AdminNoticeVO vo, HttpSession ses) {
+		//vo.setUserid((String)ses.getAttribute("userid"));
+		ModelAndView mav = new ModelAndView();
+		adminnoticeService.adminnoticeEditOk(vo);
+			mav.addObject("no", vo.getAdmin_no());
+			mav.setViewName("redirect:adminnoticeDetail");
+
+		return mav;
+	}
+	//글 삭제
+	@RequestMapping("/adminnoticeDel")
+	public ModelAndView adminnoticeDel(int admin_no, HttpSession session) {
+		String userid = "apple";
+		adminnoticeService.adminnoticeDel(admin_no, userid);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:adminnoticeMain");
+	
+		return mav;
+	}
+	
 }
